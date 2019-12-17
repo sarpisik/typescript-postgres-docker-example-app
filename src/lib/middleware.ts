b2bab0a4e889c06d21eb3acaa4849c761f7110a3
sarpisik <contact@sarpisik.com>
2019-12-17 13:57:40 +0100

    Add edit remote data merge on create movie

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,20 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import Axios, { AxiosResponse } from 'axios';
-import { MovieInterface } from './interfaces';
 import { HttpException } from './exceptions';
 import { validationResult, ErrorFormatter } from 'express-validator';
 import { capitalizeLetter } from './helpers';
 
 const apiKey = process.env.OMDBAPI_API_KEY;
-
-interface ErrorResponse {
-  Response: 'False';
-  Error: string;
-}
-
-interface SuccessResponse extends MovieInterface {
-  Response: 'True';
-}
 
 interface ValidationError {
   location?: string;
@@ -47,14 +37,14 @@ export const fetchExternalMovieDetails = async (
   next: NextFunction
 ) => {
   try {
-    const searchTitle = movie.title.replace(/\s+/g, '-');
-    const url = `http://www.omdbapi.com/?t=${searchTitle}&apikey=${apiKey}`;
-    const { data }: AxiosResponse = await Axios.get(url);
+    const slug = movie.title.replace(/\s+/g, '-');
+    const url = `http://www.omdbapi.com/?t=${slug}&apikey=${apiKey}`;
+    const { data: remoteData }: AxiosResponse = await Axios.get(url);
 
     // If external movie details exist, merge with posted movie details.
     // Else, keep posted movie details only.
     res.locals.movie =
-      data.Response !== 'False'
+      remoteData.Response !== 'False'
         ? {
             ...[
               'title',
@@ -70,10 +60,10 @@ export const fetchExternalMovieDetails = async (
               'imdbRating',
             ].reduce((movie, propertyName) => {
               const isValue = movie[propertyName];
-              // If the value exist, pass.
-              // Else, merge external value.
-              if (isValue) return movie;
-              movie[propertyName] = data[capitalizeLetter(propertyName)];
+              // If value entered by user and it is not 'title', keep it.
+              // Else, merge the external value from remote api.
+              if (isValue && propertyName !== 'title') return movie;
+              movie[propertyName] = remoteData[capitalizeLetter(propertyName)];
               return movie;
             }, movie),
           }
